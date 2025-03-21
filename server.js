1882899f48370ee620488bee7acb6cb4715bfe01
Beauti <183709979+beautisui@users.noreply.github.com>
2025-03-22 00:31:34 +0530

    build a client server connection by using steams

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
+const decodeRequest = () => {
+  return new TransformStream({
+    transform(chunk, controller) {
+      const request = JSON.parse(new TextDecoder().decode(chunk));
+      controller.enqueue(request);
+    },
+  });
+};
+
+const processRequest = () => {
+  return new TransformStream({
+    transform(chunk, controller) {
+      const responseMsg = { result: 5 };
+      controller.enqueue(responseMsg);
+    },
+  });
+};
+
+const encodeResponse = () => {
+  return new TransformStream({
+    transform(chunk, controller) {
+      controller.enqueue(new TextEncoder().encode(chunk));
+    },
+  });
+};
+
+const convertInJson = () => {
+  return new TransformStream({
+    transform(chunk, controller) {
+      controller.enqueue(JSON.stringify(chunk));
+    },
+  });
+};
 const handleConneciton = async (connection) => {
   console.log("connected to the server succesfully!!!");
-  const responseMsg = { result: 5 };
-  await connection.write(new TextEncoder().encode(JSON.stringify(responseMsg)));
-  console.log("Response sended to the client....");
+  await connection.readable
+    .pipeThrough(decodeRequest())
+    .pipeThrough(processRequest())
+    .pipeThrough(convertInJson())
+    .pipeThrough(encodeResponse())
+    .pipeTo(connection.writable);
+
+  console.log("Piping is done !");
 };
 
 const main = async (port) => {
