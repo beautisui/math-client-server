0efa8fc252b9dc4e2a94400c25df70622f7af599
Beauti <183709979+beautisui@users.noreply.github.com>
2025-03-21 23:34:11 +0530

    created a structure with test from clint side

--- client.js
+++ client.js
@@ -1,28 +1,33 @@
 import { assertEquals } from "jsr:@std/assert";
 
+const stringifyInput = (input) => {
+  const [operation, ...operands] = input.trim().split(/\s+/);
+  return JSON.stringify({ operation, operands });
+};
+
 const sendInputToServer = async (connection, input) => {
-  const requestMsg = JSON.stringify(input);
+  const requestMsg = stringifyInput(input);
   const encoded = new TextEncoder().encode(requestMsg);
   await connection.write(encoded);
 };
 
-const getResponse = async (connection) => {
-  const buff = new Uint8Array(20);
+const getResponseFromServer = async (connection) => {
+  const buff = new Uint8Array(1024);
   const size = await connection.read(buff);
   return buff.slice(0, size);
 };
 
 const test = async (connection, request, expected) => {
   await sendInputToServer(connection, request);
-  const response = await getResponse(connection);
+  const response = await getResponseFromServer(connection);
   const actualData = JSON.parse(new TextDecoder().decode(response));
 
   assertEquals(actualData, expected);
-  console.log("successfully done !");
+  console.log("All test are passed !");
 };
 
 const testClient = async (connection) => {
-  await test(connection, { operation: "Add", operand: [2, 3] }, { result: 5 });
+  await test(connection, "add 2 3", { result: 5 });
 };
 
 const main = async (port) => {
